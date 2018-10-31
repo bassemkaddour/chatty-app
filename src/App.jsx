3c989f2b2c908d4c1f5d3bafeab866c9eb7d1d38
Bassem Kaddour <bassemkaddour@gmail.com>
2018-10-31 19:46:37 +0000

    Add websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MessageList from './MessageList.jsx';
 
 import messages from '../data/messages.json';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,8 +14,9 @@ class App extends Component {
       messages: messages[0].messages
     };
     this.addMessage = this.addMessage.bind(this);
+    this.socket = new WebSocket(`ws://${window.location.hostname}:3001`); 
   }
-
+  
   componentDidMount() {
     setTimeout(() => {
       // Add a new message to the list of messages in the data store
@@ -24,6 +26,10 @@ class App extends Component {
       // Calling setState will trigger a call to render() in App and all child components.
       this.setState({messages: messages})
     }, 3000);
+    
+    this.socket.addEventListener('open', () => {
+      console.log('connected sockets');
+    });
   }
 
   addMessage(username, content) {
@@ -34,6 +40,9 @@ class App extends Component {
       username,
       content
     }
+    const jMessage = JSON.stringify(newMessage);
+    this.socket.send(jMessage);
+
     const newMessages = [...oldMessages, newMessage]; 
     this.setState({messages: newMessages})
   }
